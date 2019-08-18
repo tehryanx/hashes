67f1088eb7aadef22a83f59f9a617ec0b10eb0aa
tehryanx <ryan.cartner@gmail.com>
2019-08-18 15:27:02 -0300

    added crc32 and adler32

--- hashes.py
+++ hashes.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, zlib
 import sys
 
 if len(sys.argv) < 2:
@@ -12,10 +12,14 @@ for i in range(0, len(sys.argv)):
 
 clear = clear.strip()
 
-# loop through all available digests
+# loop through all digests available in hashlib
 
 for i in hashlib.algorithms_available:
     tab = "\t\t" if len(i)<7 else "\t"
     h = hashlib.new(i)
     h.update(clear)
     print i, tab, h.hexdigest()
+
+# adler32 and crc32 from zlib
+print "crc32", "\t\t", hex(zlib.crc32(clear))
+print "adler32", "\t", hex(zlib.adler32(clear))
